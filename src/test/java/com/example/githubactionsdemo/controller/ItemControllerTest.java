82d46652f56eb7d4e9268206340eb180387af089
April Huang <phuang@beaconfireinc.com>
2022-08-09 15:00:43 -0400

    Setup tests

---
+++ src/test/java/com/example/githubactionsdemo/controller/ItemControllerTest.java
@@ -0,0 +1,19 @@
+package com.example.githubactionsdemo.controller;
+
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ItemControllerTest {
+
+    @Test
+    void testItem() {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    void testItem2() {
+        assertEquals(2, 2);
+    }
+}
